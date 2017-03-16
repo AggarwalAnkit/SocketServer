2c47467166e58bdd458e911d69bb973f841180b8
aa <bit@treebo123>
2017-03-16 21:01:00 +0530

    parsing log message into a json object

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const LOG_FORMAT = 'date time PID TID priority tag: message'; //time contains ':' also (e.g. 20:53:19.028)
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -16,10 +18,30 @@ io.on('connection', function(socket){
   //Add a 'new message' event handler to this instance of socket
   socket.on('new message', function(msg) {
     console.log('DATA ' + socket.remoteAddress + ': ' + msg);
-    // Write the data back to the socket, the client will receive it as data from the server
-    io.emit("data", msg);
+    //parse the log message and emit the data to the sockets, the clients will receive it as data from the server
+    io.emit("data", JSON.stringify(parseLog(msg)));
   });
 });
+
+function parseLog(logMessage) {
+  var logObject = {date: "", time: "", pid: "", tid: "", priority: "", tag: "", message: ""};
+
+  var allparts = logMessage.split(" ", 6);
+  logObject.date = allparts[0];
+  logObject.time = allparts[1];
+  logObject.pid = allparts[2];
+  logObject.tid = allparts[3];
+  logObject.priority = allparts[4];
+
+  var tag = allparts[5];
+  //remove ':' from tag
+  logObject.tag = tag.replace(/\:$/, '');
+
+  //there are two colons in time part of the string and one colon after tag. So taking everything after tag
+  logObject.message = logMessage.split(":", 4)[3];
+
+  return logObject;
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
